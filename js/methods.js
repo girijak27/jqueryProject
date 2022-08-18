216e0eef2dd56da06aa22f6107c726471235b628
sakoyalkar <sai.koyalkar@copart.com>
2022-08-18 13:28:15 -0500

    matrix

--- js/methods.js
+++ js/methods.js
@@ -21,12 +21,31 @@
 
             // AJAX - Asynchornious JavaScript And XML 
 
+                $.ajax({
+                    url:'js/students.json', 
+                    method: 'get', 
+                    crossDomain: true,
+                    dataType: 'jsonp', 
+                    // data: {'phone': document.getElementById('phone').value},
+                    success: function(resp){
+                        console.log(resp);
+                       var table ="<table>";
+                       for(var i=0; i<resp.length; i++){
+                        table +="<tr><td>" + resp[i].name +"</td><td>" + resp[i].phone + "</td></tr>"
+                       }
+                       table += "</table>";
+                       $('#studentDetials').html(table);
+                    },
+                    error: function(err){
+                        console.log(err);
+                    } 
+                }); 
+
             function checkPhone(){
 
                 $.ajax({
                     url:'js/students.json', 
                     method: 'get', 
-                    // crossDomain: true,
                     dataType: 'jsonp', 
                     data: {'phone': document.getElementById('phone').value},
                     success: function(resp){
@@ -35,8 +54,49 @@
                     error: function(err){
                         console.log(err);
                     } 
-                });    
+                });
+
+                // Alternate
+
+                // $.get("js/students.json", {}, function(data, status){
+                //     alert("Data: " + data + "\nStatus: " + status);
+                // });
+
+                // $.post("demo_test.asp", {}, function(data, status){
+                //     alert("Data: " + data + "\nStatus: " + status);
+                // });
             }
 
 
+// Write a program to add, substract, and multiple two matrix
+
+function readMatrix(){
+    // var r = prompt('Enter number of rows');
+    // var c = prompt('Enter number of columns');
+    var A = [[],[],[]];
+    for(var i=0; i<3; i++){
+        for(var j=0; j<3; j++){
+            A[i][j] = prompt('Enter the value at (' + i + ',' + j +')');
+        }
+    }
+    return A;
+}
+
+var a = readMatrix();
+// var b = readMatrix();
+console.log(a);
+
+function displayMatrix(matrix){
+    var ele = "<div>";
+    for(var i=0; i<3; i++){
+        ele += "<div>";
+        for(var j=0; j<3; j++){
+            ele += "<span>" + matrix[i][j] + "</span>";
+        }
+        ele +="</div>"
+    }
+    ele +="</div>";
+    $('#matrix').html(ele);
+}
+displayMatrix(a);
 
